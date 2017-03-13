ad1041516af28ab494e7194ac7bd36beb46a18b9
elviswu <elviswu@tencent.com>
2017-03-13 12:48:17 +0800

    合入9th rsa接口

--- mars/openssl/export_include/rsa_private_decrypt.h
+++ mars/openssl/export_include/rsa_private_decrypt.h
@@ -20,7 +20,7 @@ char* 		rsa_keypair_key2string(RSA* _pkey);
 void 		ras_keypair_freekey(RSA* _key);
 void 		ras_keypair_freestring(char* _keystring);
 
-int 		rsa_private_decrypt(void** _out, unsigned int* _outlen,  const void* _in, unsigned int _inlen, RSA* _key);
+int 		rsa_private_decrypt(unsigned char** _out, unsigned int* _outlen,  const unsigned char* _in, unsigned int _inlen, RSA* _key);
 
 #ifdef __cplusplus
 }
