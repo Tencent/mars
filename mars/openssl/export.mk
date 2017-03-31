d48eb38a9eaa3535bd79a89f8c8cf7b9352a152f
elviswu <elviswu@tencent.com>
2017-03-31 14:50:24 +0800

    openssl库遗留提交

--- mars/openssl/export.mk
+++ mars/openssl/export.mk
@@ -2,19 +2,19 @@
 TEMP_LOCAL_PATH :=$(call my-dir)
 
 #include $(CLEAR_VARS)  
-#LOCAL_MODULE := crypto
-#LOCAL_SRC_FILES := $(TEMP_LOCAL_PATH)/libs/$(TARGET_ARCH_ABI)/libcrypto.so 
+#LOCAL_MODULE := cryptowrapper
+#LOCAL_SRC_FILES := $(TEMP_LOCAL_PATH)/libs/$(TARGET_ARCH_ABI)/libcryptowrapper.so 
 #LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
 #LOCAL_EXPORT_C_INCLUDES := $(TEMP_LOCAL_PATH)/src/openssl/include
 #include $(PREBUILT_SHARED_LIBRARY)
 
-ifeq ($(filter crypto,$(LOCAL_EXPORT_STATIC_LIBRARIES)),)
+ifeq ($(filter cryptowrapper,$(LOCAL_EXPORT_STATIC_LIBRARIES)),)
 include $(CLEAR_VARS)  
-LOCAL_MODULE := crypto
-LOCAL_SRC_FILES := $(TEMP_LOCAL_PATH)/obj/local/$(TARGET_ARCH_ABI)/libcrypto.a
+LOCAL_MODULE := cryptowrapper
+LOCAL_SRC_FILES := $(TEMP_LOCAL_PATH)/obj/local/$(TARGET_ARCH_ABI)/libcryptowrapper.a
 LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
 LOCAL_EXPORT_C_INCLUDES := $(TEMP_LOCAL_PATH)/export_include
 include $(PREBUILT_STATIC_LIBRARY)
 endif
 
-LOCAL_EXPORT_STATIC_LIBRARIES += crypto
+LOCAL_EXPORT_STATIC_LIBRARIES += cryptowrapper
