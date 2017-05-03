88463e37a2660f47aa075352cdc25d98ab25ad70
network0771 <akliwei@gmail.com>
2017-05-03 11:01:18 +0800

    add uwp demo

---
+++ samples/UWP/UWPDemo/App.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWPDemo.marsComponent;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UWPDemo.ui;
+
+namespace UWPDemo
+{
+    /// <summary>
+    /// 提供特定于应用程序的行为，以补充默认的应用程序类。
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// 初始化单一实例应用程序对象。这是执行的创作代码的第一行，
+        /// 已执行，逻辑上等同于 main() 或 WinMain()。
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// 在应用程序由最终用户正常启动时进行调用。
+        /// 将在启动应用程序以打开特定文件等情况下使用。
+        /// </summary>
+        /// <param name="e">有关启动请求和过程的详细信息。</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = false;
+            }
+#endif
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // 不要在窗口已包含内容时重复应用程序初始化，
+            // 只需确保窗口处于活动状态
+            if (rootFrame == null)
+            {
+                onAppFirstLaunched();
+                // 创建要充当导航上下文的框架，并导航到第一页
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: 从之前挂起的应用程序加载状态
+                }
+
+                // 将框架放在当前窗口中
+                Window.Current.Content = rootFrame;
+
+                //后退按钮
+                rootFrame.Navigated += OnNavigated;
+                SystemNavigationManager.GetForCurrentView().BackRequested += OnBackRequested;
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    rootFrame.CanGoBack ?
+                    AppViewBackButtonVisibility.Visible :
+                    AppViewBackButtonVisibility.Collapsed;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // 当导航堆栈尚未还原时，导航到第一页，
+                    // 并通过将所需信息作为导航参数传入来配置
+                    // 参数
+                    rootFrame.Navigate(typeof(ConversationPage), e.Arguments);
+                }
+                // 确保当前窗口处于活动状态
+                Window.Current.Activate();
+            }
+        }
+
+        private void OnNavigated(object sender, NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                ((Frame)sender).CanGoBack ?
+                AppViewBackButtonVisibility.Visible :
+                AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private void OnBackRequested(object sender, BackRequestedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame != null)
+            {
+                if (rootFrame.CanGoBack)
+                {
+                    e.Handled = true;
+                    rootFrame.GoBack();
+                }
+                else
+                {
+                    Application.Current.Exit();
+                }
+            }
+        }
+
+        void onAppFirstLaunched()
+        {
+            MarsComponentService.Init();
+        }
+
+        /// <summary>
+        /// 导航到特定页失败时调用
+        /// </summary>
+        ///<param name="sender">导航失败的框架</param>
+        ///<param name="e">有关导航失败的详细信息</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// 在将要挂起应用程序执行时调用。  在不知道应用程序
+        /// 无需知道应用程序会被终止还是会恢复，
+        /// 并让内存内容保持不变。
+        /// </summary>
+        /// <param name="sender">挂起的请求的源。</param>
+        /// <param name="e">有关挂起请求的详细信息。</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: 保存应用程序状态并停止任何后台活动
+            deferral.Complete();
+        }
+    }
+}
---
+++ samples/UWP/UWPDemo/src/scene/MarsPushMgr.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UWPDemo.model;
+
+namespace UWPDemo.scene
+{
+    public interface IMarsPushObserver
+    {
+        void onPush(int cmdID, MarsEventArgs args);
+    }
+    
+    public class MarsPushMgr
+    {
+        private static object sLocker = new object();
+        private static Dictionary<int, List<IMarsPushObserver>> sObserverList = new Dictionary<int, List<IMarsPushObserver>>();
+        
+        public static void addObserver(int cmdID, IMarsPushObserver obc)
+        {
+            lock (sLocker)
+            {
+                if (sObserverList.ContainsKey(cmdID))
+                {
+                    List<IMarsPushObserver> list = sObserverList[cmdID];
+                    list.Add(obc);
+                }
+                else
+                {
+                    List<IMarsPushObserver> list = new List<IMarsPushObserver>();
+                    list.Add(obc);
+                    sObserverList[cmdID] = list;
+                }
+            }
+        }
+
+        public static void delObserver(int cmdID, IMarsPushObserver obc)
+        {
+            lock (sLocker)
+            {
+                if (sObserverList.ContainsKey(cmdID))
+                {
+                    List<IMarsPushObserver> list = sObserverList[cmdID];
+                    list.Remove(obc);
+                }
+            }
+        }
+
+        public static void onPush(int cmdID, MarsEventArgs args)
+        {
+            lock (sLocker)
+            {
+                if (sObserverList.ContainsKey(cmdID))
+                {
+                    List<IMarsPushObserver> list = sObserverList[cmdID];
+                    foreach (IMarsPushObserver obc in list)
+                    {
+                        Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                        {
+                            if (obc != null)
+                            {
+                                obc.onPush(cmdID, args);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+}
