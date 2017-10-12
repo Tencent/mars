31f2bdad5eabd5d3730f7d2c6b0a664df190f2c2
garryyan <garryyan@tencent.com>
2017-10-12 18:59:04 +0800

    fix wild pointer in xlog(pub_key)

---
+++ mars/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "f685e74"
+#define MARS_PATH "master"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2017-10-12 18:37:49"
+#define MARS_TAG "mars_xlog_sdk/mars_libs"
+
+#endif
