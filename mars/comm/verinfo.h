128be0610b56f81c0f276fd1ec3cbe166919daa1
astrozhou <astrozhou@tencent.com>
2017-11-22 10:28:51 +0800

    make compile not rely on path

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "25037f1"
+#define MARS_REVISION "988f284"
 #define MARS_PATH "master"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2017-11-21 21:16:32"
-#define MARS_TAG "armeabi"
+#define MARS_BUILD_TIME "2017-11-22 10:24:56"
+#define MARS_TAG "default"
 
 #endif
