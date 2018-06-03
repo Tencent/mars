034fd1a2862a01f4083eeaf6b8522e441c7b282d
garryyan <garryyan@tencent.com>
2018-06-03 17:08:59 +0800

    add app_logic.cc into source.mk

--- mars/app/jni/sources.mk
+++ mars/app/jni/sources.mk
@@ -14,6 +14,10 @@ SRC := $(wildcard $(TEMP_LOCAL_PATH)/../src/unix/*.cc)
 SRC := $(SRC:$(LOCAL_PATH)/%=%)
 LOCAL_SRC_FILES += $(SRC)
 
+SRC := $(wildcard $(TEMP_LOCAL_PATH)/../*.cc)
+SRC := $(SRC:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(SRC)
+
 SRC := $(wildcard $(TEMP_LOCAL_PATH)/*.cc)
 SRC := $(SRC:$(LOCAL_PATH)/%=%)
 LOCAL_SRC_FILES += $(SRC)
