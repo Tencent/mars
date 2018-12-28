30fb16c3750127c2451c4f8031929d7f696dec98
astrozhou <astrozhou@tencent.com>
2018-12-28 17:59:28 +0800

    move unneed

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "45c3ce4"
+#define MARS_REVISION "57a0483"
 #define MARS_PATH "develop"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2018-12-28 16:56:55"
+#define MARS_BUILD_TIME "2018-12-28 17:54:45"
 #define MARS_TAG ""
 
 #endif
