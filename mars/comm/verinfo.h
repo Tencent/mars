5619e906ea9431f16c54a7880556b9f24f968d22
astrozhou <astrozhou@tencent.com>
2019-01-23 21:39:17 +0800

    fix assert

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "88ed5bc"
+#define MARS_REVISION "916cf60"
 #define MARS_PATH "rb/2019-JAN"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-01-23 14:46:23"
+#define MARS_BUILD_TIME "2019-01-23 15:34:54"
 #define MARS_TAG ""
 
 #endif
