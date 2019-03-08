8249354600c4cd0fd25d7da1154daefca996d5df
garryyan <garryyan@tencent.com>
2019-03-08 15:33:08 +0800

    fixed #548 Warning: header file not exist

--- mars/build_windows.py
+++ mars/build_windows.py
@@ -42,7 +42,7 @@ def build_windows(incremental, tag='', config='Release'):
     headers = dict()
     headers.update(COMM_COPY_HEADER_FILES)
     headers.update(WIN_COPY_EXT_FILES)
-    copy_file_mapping(headers, '../../', WIN_RESULT_DIR)
+    copy_file_mapping(headers, '../', WIN_RESULT_DIR)
     
     copy_windows_pdb(BUILD_OUT_PATH, sub_folders, config, WIN_LIBS_INSTALL_PATH)
 
