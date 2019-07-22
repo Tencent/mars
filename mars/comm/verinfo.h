e8e87c54ac73990c12db3ba84eb773b71694a75c
astrozhou <astrozhou@tencent.com>
2019-07-22 16:18:06 +0800

    add header of openssl

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "ee71f03"
-#define MARS_PATH "rb/2019-MAY-backup"
+#define MARS_REVISION "794887f"
+#define MARS_PATH "bugfix/20190722_openssl_64bit"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-07-02 16:02:05"
+#define MARS_BUILD_TIME "2019-07-22 16:09:55"
 #define MARS_TAG ""
 
 #endif
