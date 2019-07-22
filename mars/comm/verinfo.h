f96cf746b71756ba5ba472092a3e9ad45c5a9015
astrozhou <astrozhou@tencent.com>
2019-07-22 21:30:43 +0800

    提交 1.11c

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "e8e87c5"
+#define MARS_REVISION "40aded9"
 #define MARS_PATH "bugfix/20190722_openssl_64bit"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-07-22 17:29:27"
+#define MARS_BUILD_TIME "2019-07-22 21:26:59"
 #define MARS_TAG ""
 
 #endif
