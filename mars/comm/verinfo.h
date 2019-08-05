0a656027d978fa0e9bc1fe8823836bf2c855eebb
astrozhou <astrozhou@tencent.com>
2019-08-05 20:27:33 +0800

    build baseevent with appmodule

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "ee71f03"
-#define MARS_PATH "rb/2019-MAY-backup"
+#define MARS_REVISION "51c7056"
+#define MARS_PATH "feature/2019JUN/game_to_push"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-07-02 16:02:05"
+#define MARS_BUILD_TIME "2019-08-05 20:24:42"
 #define MARS_TAG ""
 
 #endif
