3230079a148d91202ba19f8e6878067899078b29
astrozhou <astrozhou@tencent.com>
2019-08-30 16:33:10 +0800

    fix crash

--- mars/comm/http.cc
+++ mars/comm/http.cc
@@ -416,7 +416,7 @@ bool HeaderFields::IsConnectionKeepAlive() const {
 
 uint32_t HeaderFields::KeepAliveTimeout() const {
     if(NULL == HeaderField(HeaderFields::KStringConnection))    return KDefaultKeepAliveTimeout;
-    std::string aliveConfig = HeaderField(HeaderFields::KStringKeepalive);
+    std::string aliveConfig = (NULL == HeaderField(HeaderFields::KStringKeepalive) ? "" : HeaderField(HeaderFields::KStringKeepalive));
     if(aliveConfig.length() <= 0 || aliveConfig.find(KStringKeepAliveTimeout) == std::string::npos) {
         return KDefaultKeepAliveTimeout;
     }
