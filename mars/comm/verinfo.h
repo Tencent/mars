205324dc4494a365054a05ce00abdbfcbc4dc71a
astrozhou <astrozhou@tencent.com>
2019-09-18 15:43:29 +0800

    fix smartheart bug

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "4df0d6c"
-#define MARS_PATH "master"
+#define MARS_REVISION "7b46a5b"
+#define MARS_PATH "feature/master/2019MAY_alarm_gamepush"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-09-18 11:19:47"
+#define MARS_BUILD_TIME "2019-09-18 13:12:08"
 #define MARS_TAG ""
 
 #endif
