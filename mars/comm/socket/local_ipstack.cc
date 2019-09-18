d7fe551f7674048dac8793d9e04ee69e2ca56af1
alanzyzhang <alanzyzhang@tencent.com>
2019-09-18 11:20:52 +0800

    windows localstack detect

--- mars/comm/socket/local_ipstack.cc
+++ mars/comm/socket/local_ipstack.cc
@@ -24,7 +24,6 @@
 #include <strings.h>
 #include "socket/unix_socket.h"
 #include "network/getifaddrs.h"
-#endif
 #if defined(__APPLE__)
 #include "network/getgateway.h"
 #include "network/getdnssvraddrs.h"
@@ -316,32 +315,117 @@ static void __local_info(std::string& _log) {
     _log += detail_net_info.Message();
 }
 
-// #else
-// #include <string>
-// TLocalIPStack local_ipstack_detect() {
-//     return ELocalIPStack_IPv4;
-// }
-// TLocalIPStack local_ipstack_detect_log(std::string& _log) {
-// 	_log = "no implement";
-//    return local_ipstack_detect();
-// }
 
-// #endif //__APPLE__
 
-//TIPNetworkType IPNetworkTypeDetect_Gateway() {
-//    in6_addr addr6_gateway = {0};
-//    if (0 != getdefaultgateway6(&addr6_gateway))
-//        return EIPv4;
-//    
-//    if (IN6_IS_ADDR_UNSPECIFIED(&addr6_gateway))
-//        return EIPv4;
-//    
-//    in_addr addr_gateway = {0};
-//    if (0 != getdefaultgateway(&addr_gateway))
-//        return EIPv6;
-//    
-//    if (INADDR_NONE == addr_gateway.s_addr || INADDR_ANY == addr_gateway.s_addr )
-//        return EIPv6;
-//    
-//    return EIPv46;
-//}
+
+
+#else
+#include <string>
+#include <WS2tcpip.h>
+#include <winsock.h>
+
+typedef union sockaddr_union {
+    struct sockaddr     generic;
+    struct sockaddr_in  in;
+    struct sockaddr_in6 in6;
+} sockaddr_union;
+
+/*
+ * Connect a UDP socket to a given unicast address. This will cause no network
+ * traffic, but will fail fast if the system has no or limited reachability to
+ * the destination (e.g., no IPv4 address, no IPv6 default route, ...).
+ */
+static const unsigned int kMaxLoopCount = 10;
+static int
+_test_connect(int pf, struct sockaddr *addr, size_t addrlen, struct sockaddr* local_addr, socklen_t local_addr_len) {
+    int s = socket(pf, SOCK_DGRAM, IPPROTO_UDP);
+    if (s < 0)
+        return 0;
+    int ret;
+    unsigned int loop_count = 0;
+    do {
+        ret = connect(s, addr, addrlen);
+    } while (ret < 0 && errno == EINTR && loop_count++<kMaxLoopCount);
+    if (loop_count>=kMaxLoopCount) {
+    	xerror2(TSF"connect error. loop_count = %_", loop_count);
+    }
+    int success = (ret == 0);
+    if (success) {
+        memset(local_addr, 0, sizeof(struct sockaddr_storage));
+        getsockname(s, local_addr, &local_addr_len);
+    }
+    loop_count = 0;
+    do {
+        ret = close(s);
+    } while (ret < 0 && errno == EINTR && loop_count++<kMaxLoopCount);
+    if (loop_count>=kMaxLoopCount) {
+    	xerror2(TSF"close error. loop_count = %_", loop_count);
+    }
+    return success;
+}
+
+/*
+ * The following functions determine whether IPv4 or IPv6 connectivity is
+ * available in order to implement AI_ADDRCONFIG.
+ *
+ * Strictly speaking, AI_ADDRCONFIG should not look at whether connectivity is
+ * available, but whether addresses of the specified family are "configured
+ * on the local system". However, bionic doesn't currently support getifaddrs,
+ * so checking for connectivity is the next best thing.
+ */
+static int
+_have_ipv6(struct sockaddr* local_addr, socklen_t local_addr_len) {
+
+	 static  struct sockaddr_in6 sin6_test;
+	    sin6_test.sin6_family = AF_INET6;
+	    sin6_test.sin6_port = 80;
+		sin6_test.sin6_flowinfo = 0;
+		sin6_test.sin6_scope_id = 0;
+        memset(sin6_test.sin6_addr.s6_addr, 0 , sizeof(sin6_test.sin6_addr.s6_addr));
+		// bzero(sin6_test.sin6_addr.s6_addr, sizeof(sin6_test.sin6_addr.s6_addr));
+		sin6_test.sin6_addr.s6_addr[0] = 0x20;
+	    sockaddr_union addr = { in6:sin6_test };
+
+    return _test_connect(PF_INET6, &addr.generic, sizeof(addr.in6), local_addr, local_addr_len);
+}
+
+static int
+_have_ipv4(struct sockaddr* local_addr, socklen_t local_addr_len) {
+
+    static struct sockaddr_in sin_test = {
+        sin_family:AF_INET,
+        sin_port:80,
+    };
+    sin_test.sin_addr.S_addr = htonl(0x08080808L); // 8.8.8.8
+    sockaddr_union addr = { in:sin_test };
+
+    return _test_connect(PF_INET, &addr.generic, sizeof(addr.in), local_addr, local_addr_len);
+}
+
+
+TLocalIPStack __local_ipstack_detect(std::string& _log) {
+    XMessage detail;
+    detail("local_ipstack_detect ");
+    sockaddr_storage v4_addr = {0};
+    sockaddr_storage v6_addr = {0};
+    int have_ipv4 = _have_ipv4((sockaddr*)&v4_addr, sizeof(v4_addr));
+    int have_ipv6 = _have_ipv6((sockaddr*)&v6_addr, sizeof(v6_addr));
+    int local_stack = 0;
+    if (have_ipv4) { local_stack |= ELocalIPStack_IPv4; }
+    if (have_ipv6) { local_stack |= ELocalIPStack_IPv6; }
+    
+    detail("have_ipv4:%d have_ipv6:%d \n", have_ipv4, have_ipv6);
+
+    return (TLocalIPStack)local_stack;
+}
+
+TLocalIPStack local_ipstack_detect() {
+    std::string log;
+    return __local_ipstack_detect(log);
+}
+TLocalIPStack local_ipstack_detect_log(std::string& _log) {
+	_log = "no implement";
+   return local_ipstack_detect();
+}
+
+#endif //__APPLE__
