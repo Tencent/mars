a44254c56afea00bcedd021ad2966eb28a12f83f
garryyan <garryyan@tencent.com>
2019-10-08 16:16:55 +0800

    support ndkr20

--- mars/build_android.py
+++ mars/build_android.py
@@ -160,7 +160,7 @@ if __name__ == '__main__':
             main(False, archs, tag=sys.argv[1])
             break
         else:
-            archs = set(['armeabi'])
+            archs = set(['armeabi-v7a', 'arm64-v8a'])
             num = raw_input('Enter menu:\n1. Clean && build mars.\n2. Build incrementally mars.\n3. Clean && build xlog.\n4. Exit\n')
             if num == '1':
                 main(False, archs)
