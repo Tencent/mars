81da38cdb56a8f89769671b04260dc5a86d41cb0
garryyan <garryyan@tencent.com>
2019-10-22 16:10:04 +0800

    fixed #711 build fail

--- mars/comm/platform_comm.h
+++ mars/comm/platform_comm.h
@@ -46,7 +46,7 @@ struct WifiInfo {
     std::string ssid;
     std::string bssid;
 };
-bool getCurWifiInfo(WifiInfo& _wifi_info);
+bool getCurWifiInfo(WifiInfo& _wifi_info, bool _force_refresh = false);
 
 struct SIMInfo {
     std::string isp_code;
