6fbea5f1dd85a342bb42f72a42cd1a1bf8f4081c
astrozhou <astrozhou@tencent.com>
2019-10-23 20:43:36 +0800

    Squashed commit of the following:
    commit 2ddc804f9106b8e74fc49052dbffefbb0885ab8c
    Author: astrozhou <astrozhou@tencent.com>
    Date:   Tue Oct 22 17:34:40 2019 +0800
    
        add
    
    commit 8d87662fb1904fdde9234facc82065969acdf555
    Author: astrozhou <astrozhou@tencent.com>
    Date:   Tue Oct 22 17:34:15 2019 +0800
    
        uni jun

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "9169f92"
-#define MARS_PATH "bugfix/2019-JUN/premerge_gamepush"
+#define MARS_REVISION "548a521"
+#define MARS_PATH "rb/2019-JUN"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-09-18 18:10:26"
+#define MARS_BUILD_TIME "2019-10-22 17:31:33"
 #define MARS_TAG ""
 
 #endif
