d0a933fc2619cfbb09f522d7d4006c3738fdc87c
astrozhou <astrozhou@tencent.com>
2019-10-30 10:00:14 +0800

    add title for callback

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "0a65602"
-#define MARS_PATH "rb/2019-JUN"
+#define MARS_REVISION "5bb7568"
+#define MARS_PATH "rb/2019-OCT"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-08-28 15:47:49"
-#define MARS_TAG "test"
+#define MARS_BUILD_TIME "2019-10-29 20:23:10"
+#define MARS_TAG ""
 
 #endif
