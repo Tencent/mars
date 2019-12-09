b41333cb94ec8d066725291d03a2a51490dd0fc0
perryzhou <perryzhou@tencent.com>
2019-12-09 16:27:23 +0800

    autobuffer 的最大buffer放到50M

--- mars/comm/autobuffer.cc
+++ mars/comm/autobuffer.cc
@@ -311,7 +311,7 @@ void AutoBuffer::__FitSize(size_t _len) {
 
         parray_ = (unsigned char*) p;
 
-        ASSERT2(_len <= 20 * 1024 * 1024, "%u", (uint32_t)_len);
+        ASSERT2(_len <= 50 * 1024 * 1024, "%u", (uint32_t)_len);
         ASSERT(parray_);
         
         memset(parray_+capacity_, 0, mallocsize-capacity_);
