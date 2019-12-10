59c975418a981e97bee84962cbfe7618fa7fc3d7
perryzhou <perryzhou@tencent.com>
2019-12-10 19:36:58 +0800

    verinfo.h 加回去

---
+++ mars/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "2380815f"
+#define MARS_PATH "rb/2019-DEC"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2019-12-10 17:01:30"
+#define MARS_TAG ""
+
+#endif
