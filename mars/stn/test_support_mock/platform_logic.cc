5a2f3323c3bacb989f1bd97185ce7eaa2c7684f0
felixzhou <felixzhou@tencent.com>
2019-12-17 20:48:42 +0800

    删除getKVCommPath

--- mars/stn/test_support_mock/platform_logic.cc
+++ mars/stn/test_support_mock/platform_logic.cc
@@ -48,9 +48,9 @@ void getWatchDogPath(std::string& aPath)
 {
 }
 
-void getKVCommPath(std::string& aPath)
-{
-}
+// void getKVCommPath(std::string& aPath)
+// {
+// }
 
 
 void getCrashFilePath(std::string& filePath, int timespan) {
