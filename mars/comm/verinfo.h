c791ee96be77cc6930ea388dd5109fa1492be833
perryzhou <perryzhou@tencent.com>
2019-12-18 16:25:25 +0800

    [android] [openssl] rebuild openssl with ndk20 & abi 21

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "70e0c2d4"
+#define MARS_REVISION "b59f9feb"
 #define MARS_PATH "rb/2019-DEC"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-12-18 15:12:01"
+#define MARS_BUILD_TIME "2019-12-18 16:23:26"
 #define MARS_TAG ""
 
 #endif
