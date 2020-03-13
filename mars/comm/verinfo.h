8e112ff261435309f18eeb5a4ab20e046421eb59
astrozhou <astrozhou@tencent.com>
2020-03-13 09:50:25 +0800

    add abort test

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "a614828e"
+#define MARS_REVISION "cacf3904"
 #define MARS_PATH "feature/feb_merge_multilonglink"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-03-11 09:30:55"
+#define MARS_BUILD_TIME "2020-03-13 09:48:48"
 #define MARS_TAG ""
 
 #endif
