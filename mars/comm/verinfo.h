f0fddbb9a656c29438f8856a78aa9fdff05b4be2
astrozhou <astrozhou@tencent.com>
2020-04-23 15:07:22 +0800

    fix longlink release crash

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "16acc6a4"
-#define MARS_PATH "rb/2020-APR"
+#define MARS_REVISION "66a9e7f3"
+#define MARS_PATH "ios_master"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-03-30 18:28:57"
-#define MARS_TAG ""
+#define MARS_BUILD_TIME "2020-04-23 14:28:32"
+#define MARS_TAG "localtest"
 
 #endif
