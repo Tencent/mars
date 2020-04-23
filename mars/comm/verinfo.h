66a3edc56f521f327c63bf91eb216d7b0fe2f65a
perryzhou <perryzhou@tencent.com>
2020-04-23 20:42:11 +0800

    version file

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "78a4409b"
-#define MARS_PATH "rb/2020-APR"
+#define MARS_REVISION "1fc14141"
+#define MARS_PATH "rb/2020-MAY"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-04-21 15:07:13"
+#define MARS_BUILD_TIME "2020-04-23 19:42:08"
 #define MARS_TAG ""
 
 #endif
