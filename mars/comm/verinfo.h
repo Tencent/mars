0cbf191e939c9f43c433d57d5d4e937508cb2291
visonhuang <visonhuang@tencent.com>
2020-07-07 21:09:11 +0800

    version

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "d7787efd"
-#define MARS_PATH "feature/2020-MAY/xlog_zstd"
+#define MARS_REVISION "1074903c"
+#define MARS_PATH "rb/2020-JUL"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-05-22 11:04:58"
+#define MARS_BUILD_TIME "2020-07-07 21:06:43"
 #define MARS_TAG ""
 
 #endif
