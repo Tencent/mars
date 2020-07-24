fa33829e868c7c23f30e9fa0b862446ff44b5214
perryzhou <perryzhou@tencent.com>
2020-07-24 15:24:28 +0800

    android compiler

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "1074903c"
-#define MARS_PATH "rb/2020-JUL"
+#define MARS_REVISION "c58074b3"
+#define MARS_PATH "master"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-07-07 21:06:43"
+#define MARS_BUILD_TIME "2020-07-24 15:11:06"
 #define MARS_TAG ""
 
 #endif
