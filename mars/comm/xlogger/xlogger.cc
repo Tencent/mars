444c9c0d41af28b9a384cf84a032e0681c94af0c
astrozhou <astrozhou@tencent.com>
2020-07-24 11:03:41 +0800

    部分实现移入 cc 文件

--- mars/comm/xlogger/xlogger.cc
+++ mars/comm/xlogger/xlogger.cc
@@ -31,3 +31,137 @@ XLogger::~XLogger() {
 	else
 		xlogger_Write(m_isinfonull?NULL:&m_info, m_message.c_str());
 }
+
+void XLogger::operator>> (XLogger& _xlogger) {
+	if (_xlogger.m_info.level < m_info.level)
+	{
+		_xlogger.m_info.level = m_info.level;
+		_xlogger.m_isassert = m_isassert;
+		_xlogger.m_exp = m_exp;
+	}
+
+	m_isassert = false;
+	m_exp = NULL;
+
+	_xlogger.m_message += m_message;
+	m_message.clear();
+}
+
+void XLogger::operator<< (XLogger& _xlogger) {
+	_xlogger.operator>>(*this);
+}
+
+XLogger& XLogger::Assert(const char* _exp) {
+	m_isassert = true;
+	m_exp = _exp;
+	return *this;
+}
+
+XLogger& XLogger::operator<< (const string_cast& _value) {
+	if (NULL != _value.str()) {
+		m_message += _value.str();
+	} else {
+		m_info.level = kLevelFatal;
+		m_message += "{!!! XLogger& XLogger::operator<<(const string_cast& _value): _value.str() == NULL !!!}";
+		assert(false);
+	}
+	return *this;
+}
+
+XLogger& XLogger::operator>>(const string_cast& _value) {
+	if (NULL != _value.str()) {
+		m_message.insert(0,  _value.str());
+	} else {
+		m_info.level = kLevelFatal;
+		m_message.insert(0,  "{!!! XLogger& XLogger::operator>>(const string_cast& _value): _value.str() == NULL !!!}");
+		assert(false);
+	}
+	return *this;
+}
+
+XLogger& XLogger::VPrintf(const char* _format, va_list _list) {
+	if (_format == NULL)
+	{
+		m_info.level = kLevelFatal;
+		m_message += "{!!! XLogger& XLogger::operator()(const char* _format, va_list _list): _format == NULL !!!}";
+		assert(false);
+		return *this;
+	}
+
+	char temp[4096] = {'\0'};
+	vsnprintf(temp, 4096, _format, _list);
+	m_message += temp;
+	return *this;
+}
+
+XLogger& XLogger::operator()(const char* _format, ...) {
+	if (_format == NULL)
+	{
+		m_info.level = kLevelFatal;
+		m_message += "{!!! XLogger& XLogger::operator()(const char* _format, ...): _format == NULL !!!}";
+		assert(false);
+		return *this;
+	}
+
+	va_list valist;
+	va_start(valist, _format);
+	VPrintf(_format, valist);
+	va_end(valist);
+	return *this;
+}
+
+void XLogger::DoTypeSafeFormat(const char* _format, const string_cast** _args) {
+
+	const char* current = _format;
+	int count = 0;
+	while ('\0' != *current)
+	{
+		if ('%' != *current)
+		{
+			m_message += *current;
+			++current;
+			continue;
+		}
+
+		char nextch = *(current+1);
+		if (('0' <=nextch  && nextch <= '9') || nextch == '_')
+		{
+
+			int argIndex = count;
+			if (nextch != '_') argIndex = nextch - '0';
+
+			if (_args[argIndex] != NULL)
+			{
+				if (NULL != _args[argIndex]->str())
+				{
+					m_message += _args[argIndex]->str();
+				} else {
+					m_info.level = kLevelFatal;
+					m_message += "{!!! void XLogger::DoTypeSafeFormat: _args[";
+					m_message += string_cast(argIndex).str();
+					m_message += "]->str() == NULL !!!}";
+					assert(false);
+				}
+			} else {
+				m_info.level = kLevelFatal;
+				m_message += "{!!! void XLogger::DoTypeSafeFormat: _args[";
+				m_message += string_cast(argIndex).str();
+				m_message += "] == NULL !!!}";
+				assert(false);
+			}
+			count++;
+			current += 2;
+		}
+		else if (nextch == '%') {
+			m_message += '%';
+			current += 2;
+		} else {
+			++current;
+			m_info.level = kLevelFatal;
+			m_message += "{!!! void XLogger::DoTypeSafeFormat: %";
+			m_message += nextch;
+			m_message += " not fit mode !!!}";
+			assert(false);
+		}
+	}
+}
