9ab9042abfbd0c22d64c4dd3a6808137f6428731
GkvJwa <285794886@qq.com>
2020-08-01 22:49:01 +0800

    add use_compress

--- mars/log/src/appender.cc
+++ mars/log/src/appender.cc
@@ -924,19 +924,20 @@ void appender_open(const XLogConfig& _config) {
     snprintf(mmap_file_path, sizeof(mmap_file_path), "%s/%s.mmap3", sg_cache_logdir.empty() ? _config.logdir_.c_str() : sg_cache_logdir.c_str(), _config.nameprefix_.c_str());
 
     bool use_mmap = false;
+	bool use_compress = true;
     if (OpenMmapFile(mmap_file_path, kBufferBlockLength, sg_mmmap_file))  {
         if (_config.compress_mode_ == kZstd){
-            sg_log_buff = new LogZstdBuffer(sg_mmmap_file.data(), kBufferBlockLength, true, _config.pub_key_.c_str(), _config.compress_level_);
+            sg_log_buff = new LogZstdBuffer(sg_mmmap_file.data(), kBufferBlockLength, use_compress, _config.pub_key_.c_str(), _config.compress_level_);
         }else {
-            sg_log_buff = new LogZlibBuffer(sg_mmmap_file.data(), kBufferBlockLength, true, _config.pub_key_.c_str());
+            sg_log_buff = new LogZlibBuffer(sg_mmmap_file.data(), kBufferBlockLength, use_compress, _config.pub_key_.c_str());
         }
         use_mmap = true;
     } else {
         char* buffer = new char[kBufferBlockLength];
         if (_config.compress_mode_ == kZstd){
-            sg_log_buff = new LogZstdBuffer(buffer, kBufferBlockLength, true, _config.pub_key_.c_str(), _config.compress_level_);
+            sg_log_buff = new LogZstdBuffer(buffer, kBufferBlockLength, use_compress, _config.pub_key_.c_str(), _config.compress_level_);
         } else {
-            sg_log_buff = new LogZlibBuffer(buffer, kBufferBlockLength, true, _config.pub_key_.c_str());
+            sg_log_buff = new LogZlibBuffer(buffer, kBufferBlockLength, use_compress, _config.pub_key_.c_str());
         }
         use_mmap = false;
     }
