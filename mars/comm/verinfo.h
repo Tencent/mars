1b1add50a9b696c5f4d5ed295d0ecb1899746a45
perryzhou <perryzhou@tencent.com>
2020-08-26 20:50:04 +0800

    use socket_storage save socket address

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "7e70c931"
-#define MARS_PATH "rb/2020-AUG"
+#define MARS_REVISION "b3839cfe"
+#define MARS_PATH "rb/2020-SEP"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-08-11 12:44:41"
+#define MARS_BUILD_TIME "2020-08-26 19:29:19"
 #define MARS_TAG ""
 
 #endif
