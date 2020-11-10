8e9ab8aa6c711954a54336b0075c89e34c75cc29
garryyan <garryyan@tencent.com>
2020-11-10 16:26:48 +0800

    try to fix crash in `__sfvwrite`

--- mars/log/src/formater.cc
+++ mars/log/src/formater.cc
@@ -37,6 +37,50 @@
 #include <inttypes.h>
 #endif
 
+#if !defined(ANDROID) && !_WIN32
+static char* reverse(char *str, int len) {
+    char* p1 = str;
+    char* p2 = str + len - 1;
+
+    while (p1 < p2) {
+        char tmp = *p1;
+        *p1++ = *p2;
+        *p2-- = tmp;
+    }
+
+    return str;
+}
+
+static int logger_itoa(int num, char* str, int len, int min) {
+    if (NULL == str || len == 0) {
+        return 0;
+    }
+
+    int sign = num;
+    if (sign < 0) {
+        num = -num;
+        len--;
+        min--;
+    }
+
+    int i = 0;
+    do {
+        str[i++] = '0' + num % 10;
+        num /= 10;
+    } while (num && (i < (len - 1)));
+
+    while (i < min && i < (len - 1)) {
+        str[i++] = '0';
+    }
+    if (sign < 0) {
+        str[i++] = '-';
+    }
+    str[i] = '\0';
+    reverse(str, i);
+    return i;
+}
+#endif
+
 void log_formater(const XLoggerInfo* _info, const char* _logbody, PtrBuffer& _log) {
     static const char* levelStrings[] = {
         "V",
@@ -93,8 +137,49 @@ void log_formater(const XLoggerInfo* _info, const char* _logbody, PtrBuffer& _lo
             snprintf(temp_time, sizeof(temp_time), "%d-%02d-%02d +%.3s %02d:%02d:%02d.%.3d", 1900 + tm.tm_year, 1 + tm.tm_mon, tm.tm_mday,
                      (-_timezone) / 3600.0, tm.tm_hour, tm.tm_min, tm.tm_sec, _info->timeval.tv_usec / 1000);
 #else
-            snprintf(temp_time, sizeof(temp_time), "%d-%02d-%02d +%.3s %02d:%02d:%02d.%.3d", 1900 + tm.tm_year, 1 + tm.tm_mon, tm.tm_mday,
-                     gmt.c_str(), tm.tm_hour, tm.tm_min, tm.tm_sec, _info->timeval.tv_usec / 1000);
+            do {
+                int len = 0;
+                int total_len = sizeof(temp_time);
+                len += logger_itoa(1900 + tm.tm_year, temp_time + len, total_len - len, 4);
+                if (len >= total_len - 1) {
+                    break;
+                }
+                temp_time[len++] = '-';
+                len += logger_itoa(1 + tm.tm_mon, temp_time + len, total_len - len, 2);
+                if (len >= total_len - 1) {
+                    break;
+                }
+                temp_time[len++] = '-';
+                len += logger_itoa(tm.tm_mday, temp_time + len, total_len - len, 2);
+                if (len >= total_len - 2) {
+                    break;
+                }
+                temp_time[len++] = ' ';
+                if (tm.tm_gmtoff > 0) {
+                    temp_time[len++] = '+';
+                }
+                len += logger_itoa(tm.tm_gmtoff / 360, temp_time + len, total_len - len, 0);
+                if (len >= total_len - 1) {
+                    break;
+                }
+                temp_time[len++] = ' ';
+                len += logger_itoa(tm.tm_hour, temp_time + len, total_len - len, 2);
+                if (len >= total_len - 1) {
+                    break;
+                }
+                temp_time[len++] = ':';
+                len += logger_itoa(tm.tm_min, temp_time + len, total_len - len, 2);
+                if (len >= total_len - 1) {
+                    break;
+                }
+                temp_time[len++] = ':';
+                len += logger_itoa(tm.tm_sec, temp_time + len, total_len - len, 2);
+                if (len >= total_len - 1) {
+                    break;
+                }
+                temp_time[len++] = '.';
+                len += logger_itoa(_info->timeval.tv_usec / 1000, temp_time + len, total_len - len, 3);
+            } while (false);
 #endif
         }
 
