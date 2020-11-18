f03f3ac57c14e9773458da640f791eb690ad8992
astrozhou <astrozhou@tencent.com>
2020-11-18 11:21:27 +0800

    compile fix

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "cc99bd80"
+#define MARS_REVISION "a03e9df3"
 #define MARS_PATH "feature/ilink_ae/min_version2"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-11-13 21:59:01"
+#define MARS_BUILD_TIME "2020-11-18 11:15:40"
 #define MARS_TAG ""
 
 #endif
