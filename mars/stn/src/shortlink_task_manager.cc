04381f88aec4ef408cfd1a5859eb6c62594e80e3
astrozhou <astrozhou@tencent.com>
2020-11-30 21:17:33 +0800

    修复误删的代码

--- mars/stn/src/shortlink_task_manager.cc
+++ mars/stn/src/shortlink_task_manager.cc
@@ -315,6 +315,10 @@ void ShortLinkTaskManager::__RunOnStartTask() {
 
         first->use_proxy =  (first->remain_retry_count == 0 && first->task.retry_count > 0) ? !default_use_proxy_ : default_use_proxy_;
         ShortLinkInterface* worker = ShortLinkChannelFactory::Create(MessageQueue::Handler2Queue(asyncreg_.Get()), net_source_, first->task, first->use_proxy);
+        worker->OnSend.set(std::bind(&ShortLinkTaskManager::__OnSend, this, _1), worker, AYNC_HANDLER);
+        worker->OnRecv.set(std::bind(&ShortLinkTaskManager::__OnRecv, this, _1, _2, _3), worker, AYNC_HANDLER);
+        worker->OnResponse.set(std::bind(&ShortLinkTaskManager::__OnResponse, this, _1, _2, _3, _4, _5, _6, _7), worker, AYNC_HANDLER);
+        worker->GetCacheSocket = std::bind(&ShortLinkTaskManager::__OnGetCacheSocket, this, _1);
         first->running_id = (intptr_t)worker;
 
         xassert2(worker && first->running_id);
