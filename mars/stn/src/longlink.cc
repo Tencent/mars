43776dc5304be23efee281fd63062e7692c8ab14
zuyuanzhang <zuyuanzhang@qq.com>
2021-03-23 21:04:52 +0800

    add log

--- mars/stn/src/longlink.cc
+++ mars/stn/src/longlink.cc
@@ -294,7 +294,7 @@ bool LongLink::__NoopReq(XLogger& _log, Alarm& _alarm, bool need_active_timeout)
         xinfo2(TSF"start noop synccheck taskid:%0, cmdid:%1, ", Task::kLongLinkIdentifyCheckerTaskID, req_cmdid) >> _log;
     } else {
         suc = __SendNoopWhenNoData();
-        xinfo2(TSF"start noop taskid:%0, cmdid:%1, ", Task::kNoopTaskID, longlink_noop_cmdid()) >> _log;
+        xinfo2(TSF"start noop taskid:%0, cmdid:%1, suc: %_", Task::kNoopTaskID, longlink_noop_cmdid(), suc) >> _log;
     }
     
     if (suc) {
