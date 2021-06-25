1939fb2e79016b5e664915dd6c8130de17477bc7
alanzyzhang <alanzyzhang@tencent.com>
2021-06-25 21:28:53 +0800

    build windows

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "feb3ecbd"
-#define MARS_PATH "master"
+#define MARS_REVISION "40844c2c"
+#define MARS_PATH "alanzyzhang/master/windows_vs2019"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-01-12 15:03:47"
+#define MARS_BUILD_TIME "2021-01-15 14:44:55"
 #define MARS_TAG ""
 
 #endif
