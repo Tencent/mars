306c6948354969b5aec5d776c0244fdb699f0183
perryzhou <perryzhou@tencent.com>
2021-09-24 17:51:01 +0800

    windows pdb files

--- mars/mars_utils.py
+++ mars/mars_utils.py
@@ -187,13 +187,13 @@ def clean_windows(path, incremental):
         
 def copy_windows_pdb(cmake_out, sub_folder, config, dst_folder):
     for sf in sub_folder:
-        src_file = "%s/%s/" %(cmake_out, sf)
-        dirs = glob.glob(src_file + "*.dir")
-        if len(dirs) != 1:
-            print("Warning: %s path error." %src_file)
-            continue
+        # src_file = "%s/%s/" %(cmake_out, sf)
+        # dirs = glob.glob(src_file + "*.dir")
+        # if len(dirs) != 1:
+        #     print("Warning: %s path error." %src_file)
+        #     continue
         
-        src_file = "%s/%s" %(dirs[0], config)
+        src_file = "%s/%s/%s" %(cmake_out, sf, config)
         pdbs = glob.glob(src_file + "/*.pdb")
         if len(pdbs) != 1:
             print("Warning: %s path error." %src_file)
