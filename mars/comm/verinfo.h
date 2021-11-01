32a402499d5d9545409f7099f9afa26958495822
alanzyzhang <alanzyzhang@tencent.com>
2021-11-01 14:49:53 +0800

    longlink add task intercepter

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "ae208c33"
+#define MARS_REVISION "699257c1"
 #define MARS_PATH "rb/2021-NOV"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-10-29 16:15:12"
+#define MARS_BUILD_TIME "2021-11-01 14:41:46"
 #define MARS_TAG ""
 
 #endif
