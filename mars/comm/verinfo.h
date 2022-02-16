88c09288a0a95196b14c7cd40362e996b1349adc
perryzhou <perryzhou@tencent.com>
2022-02-16 20:30:03 +0800

    rewrite socketbreaker, add unittest

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "54969ba5"
+#define MARS_REVISION "957523bc"
 #define MARS_PATH "rb/2022-FEB"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-02-15 15:44:41"
+#define MARS_BUILD_TIME "2022-02-16 20:28:14"
 #define MARS_TAG ""
 
 #endif
