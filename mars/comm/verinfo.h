460b8208e19e8226803cc40d5b1db57b6bbd155b
perryzhou <perryzhou@tencent.com>
2022-03-23 20:43:24 +0800

    unittest

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "4ac65b74"
-#define MARS_PATH "rb/2022-FEB"
+#define MARS_REVISION "617af1dd"
+#define MARS_PATH "rb/2022-MAR"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-02-22 19:24:10"
+#define MARS_BUILD_TIME "2022-03-23 20:36:58"
 #define MARS_TAG ""
 
 #endif
