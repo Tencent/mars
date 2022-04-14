e326931e75eb72539179c5799e41530091aec00f
garryyan <garryyan@tencent.com>
2022-04-14 14:39:40 +0800

    delete unused code

--- mars/libraries/mars_android_sdk/src/main/java/com/tencent/mars/xlog/Log.java
+++ mars/libraries/mars_android_sdk/src/main/java/com/tencent/mars/xlog/Log.java
@@ -370,35 +370,35 @@ public class Log {
         }
     }
 
-    private static final String SYS_INFO;
+    // private static final String SYS_INFO;
 
-    static {
-        final StringBuilder sb = new StringBuilder();
-        try {
-            sb.append("VERSION.RELEASE:[" + android.os.Build.VERSION.RELEASE);
-            sb.append("] VERSION.CODENAME:[" + android.os.Build.VERSION.CODENAME);
-            sb.append("] VERSION.INCREMENTAL:[" + android.os.Build.VERSION.INCREMENTAL);
-            sb.append("] BOARD:[" + android.os.Build.BOARD);
-            sb.append("] DEVICE:[" + android.os.Build.DEVICE);
-            sb.append("] DISPLAY:[" + android.os.Build.DISPLAY);
-            sb.append("] FINGERPRINT:[" + android.os.Build.FINGERPRINT);
-            sb.append("] HOST:[" + android.os.Build.HOST);
-            sb.append("] MANUFACTURER:[" + android.os.Build.MANUFACTURER);
-            sb.append("] MODEL:[" + android.os.Build.MODEL);
-            sb.append("] PRODUCT:[" + android.os.Build.PRODUCT);
-            sb.append("] TAGS:[" + android.os.Build.TAGS);
-            sb.append("] TYPE:[" + android.os.Build.TYPE);
-            sb.append("] USER:[" + android.os.Build.USER + "]");
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+    // static {
+    //     final StringBuilder sb = new StringBuilder();
+    //     try {
+    //         sb.append("VERSION.RELEASE:[" + android.os.Build.VERSION.RELEASE);
+    //         sb.append("] VERSION.CODENAME:[" + android.os.Build.VERSION.CODENAME);
+    //         sb.append("] VERSION.INCREMENTAL:[" + android.os.Build.VERSION.INCREMENTAL);
+    //         sb.append("] BOARD:[" + android.os.Build.BOARD);
+    //         sb.append("] DEVICE:[" + android.os.Build.DEVICE);
+    //         sb.append("] DISPLAY:[" + android.os.Build.DISPLAY);
+    //         sb.append("] FINGERPRINT:[" + android.os.Build.FINGERPRINT);
+    //         sb.append("] HOST:[" + android.os.Build.HOST);
+    //         sb.append("] MANUFACTURER:[" + android.os.Build.MANUFACTURER);
+    //         sb.append("] MODEL:[" + android.os.Build.MODEL);
+    //         sb.append("] PRODUCT:[" + android.os.Build.PRODUCT);
+    //         sb.append("] TAGS:[" + android.os.Build.TAGS);
+    //         sb.append("] TYPE:[" + android.os.Build.TYPE);
+    //         sb.append("] USER:[" + android.os.Build.USER + "]");
+    //     } catch (Throwable e) {
+    //         e.printStackTrace();
+    //     }
 
-        SYS_INFO = sb.toString();
-    }
+    //     SYS_INFO = sb.toString();
+    // }
 
-    public static String getSysInfo() {
-        return SYS_INFO;
-    }
+    // public static String getSysInfo() {
+    //     return SYS_INFO;
+    // }
 
     private static Map<String, LogInstance> sLogInstanceMap = new HashMap<>();
 
