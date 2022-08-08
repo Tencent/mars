f6f5eb087c8b1521f524265576084d2db6441a13
garryyan <1173828173@qq.com>
2022-08-08 16:17:18 +0800

    去掉对 openssl 的依赖，注意这里如果和 openssl 一起用可能会有内存泄露

--- mars/comm/windows/thread/thread.h
+++ mars/comm/windows/thread/thread.h
@@ -23,7 +23,7 @@
 #include "assert/__assert.h"
 #include "condition.h"
 #include "thread/runnable.h"
-#include "mars/openssl/include/openssl/crypto.h"
+//#include "mars/openssl/include/openssl/crypto.h"
 
 typedef HANDLE  thread_handler;
 #define thrd_success (0)
@@ -356,7 +356,7 @@ class Thread {
 
     static void cleanup(void* arg) {
         // cleanup tls alloctions for openssl.
-        OPENSSL_thread_stop();
+//        OPENSSL_thread_stop();
 
         volatile RunnableReference* runableref = static_cast<RunnableReference*>(arg);
         ScopedSpinLock lock((const_cast<RunnableReference*>(runableref))->splock);
