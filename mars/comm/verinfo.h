1797bdfbf071b81d3839b53998d9a9892dc4966d
perryzhou <perryzhou@tencent.com>
2022-08-26 09:51:48 +0800

    reduce log

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "0ae833bd"
+#define MARS_REVISION "73e36241"
 #define MARS_PATH "rb/2022-09"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-08-23 18:33:16"
+#define MARS_BUILD_TIME "2022-08-25 21:24:55"
 #define MARS_TAG ""
 
 #endif
