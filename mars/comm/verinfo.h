4f921ef7cfb0178e69e29230d1ea212493e088b7
cpan <cpan@tencent.com>
2022-09-29 10:18:29 +0800

    [mmnet] cdn backup

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "f69a8e1d"
+#define MARS_REVISION "473b4a29"
 #define MARS_PATH "cpan/2022-08/mars2-1"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-09-27 11:35:12"
+#define MARS_BUILD_TIME "2022-09-28 21:26:40"
 #define MARS_TAG ""
 
 #endif
