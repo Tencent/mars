2c79bb714aa6934411f98ee18c472c028f7981f3
perryzhou <perryzhou@tencent.com>
2022-11-01 17:57:20 +0800

    fix build

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "8ef9501e"
+#define MARS_REVISION "8e75e6a7"
 #define MARS_PATH "master"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-11-01 15:12:51"
+#define MARS_BUILD_TIME "2022-11-01 17:51:41"
 #define MARS_TAG ""
 
 #endif
