24bf935b7e75eb16964589d79896b8a9b46cdccb
cpan <cpan@tencent.com>
2023-01-05 16:48:32 +0800

    [mmnet]stn_manager_wrapper

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "2a862416"
+#define MARS_REVISION "886d2d89"
 #define MARS_PATH "cpan/2022-08/mars2-2"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-01-03 15:58:01"
+#define MARS_BUILD_TIME "2023-01-05 16:28:44"
 #define MARS_TAG ""
 
 #endif
