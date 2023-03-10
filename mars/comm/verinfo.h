ff88aad1ad1c419f896ed59941820ce68a585dfa
cpan <cpan@tencent.com>
2023-03-10 15:48:33 +0800

    [mars2]修改全局变量为类成员

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "36cfc929"
+#define MARS_REVISION "08a7960d"
 #define MARS_PATH "cpan/2023-01/mars2"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-03-10 15:00:21"
+#define MARS_BUILD_TIME "2023-03-10 15:37:50"
 #define MARS_TAG ""
 
 #endif
