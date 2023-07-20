57fc90ceb643934e5b0d5c2c83d2016cd77114cc
perryzhou <perryzhou@tencent.com>
2023-07-20 19:41:58 +0800

    去掉不必要的长连接是否存在判断

--- mars/stn/src/net_core.cc
+++ mars/stn/src/net_core.cc
@@ -313,12 +313,12 @@ void NetCore::StartTask(const Task& _task) {
     if (need_use_longlink_) {
         longlink = longlink_task_manager_->GetLongLink(task.channel_name);
     }
-    if ((task.channel_select == Task::kChannelLong || task.channel_select == Task::kChannelMinorLong) && (!longlink || !longlink->IsConnected())){
-        //.必须长链或副长链，但指定连接不存在，则回调失败.
-        xerror2(TSF"err no longlink (%_, %_), ", kEctLocal, kEctLocalLongLinkUnAvailable) >> group;
-        OnTaskEnd(task.taskid, task.user_context, task.user_id, kEctLocal, kEctLocalLongLinkUnAvailable, ConnectProfile());
-        return;
-    }
+    // if ((task.channel_select == Task::kChannelLong || task.channel_select == Task::kChannelMinorLong) && (!longlink || !longlink->IsConnected())){
+    //     //.必须长链或副长链，但指定连接不存在，则回调失败.
+    //     xerror2(TSF"err no longlink (%_, %_), ", kEctLocal, kEctLocalLongLinkUnAvailable) >> group;
+    //     OnTaskEnd(task.taskid, task.user_context, task.user_id, kEctLocal, kEctLocalLongLinkUnAvailable, ConnectProfile());
+    //     return;
+    // }
 
     std::shared_ptr<LongLinkMetaData> minorlonglink = nullptr;
     if((task.channel_select & Task::kChannelMinorLong) && !task.minorlong_host_list.empty()) {
