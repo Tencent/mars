417436431304100585759abd112b8ccfd019ddf7
tiemuhuaguo <tiemuhuaguo@tencent.com>
2023-10-10 11:03:54 +0800

    windows中的socket breaker单独拆分到一个文件中

--- mars/comm/windows/SocketSelect/SocketSelect2.h
+++ mars/comm/windows/SocketSelect/SocketSelect2.h
@@ -18,52 +18,11 @@
 
 #include "socket/unix_socket.h"
 #include "thread/lock.h"
-
 #include "mars/comm/xlogger/xlogger.h"
+#include "socketbreaker.h"
 
 namespace mars {
 namespace comm {
-
-class SocketSelect;
-class SocketBreaker {
-    friend SocketSelect;
-
- public:
-    SocketBreaker();
-    ~SocketBreaker();
-
-    bool IsCreateSuc() const;
-    bool ReCreate();
-    void Close();
-
-    bool Break();
-    bool Break(int reason);
-    bool Clear();
-    bool PreciseBreak(uint32_t cookie) {
-        xassert2(false, "PreciseBreak hasn't implemented on windows");
-        return false;
-    }
-    bool PreciseClear(uint32_t* cookie) {
-        xassert2(false, "PreciseClear hasn't implemented on windows");
-        return false;
-    }
-
-    bool IsBreak() const;
-    WSAEVENT BreakerFD() const;
-    int BreakReason() const;
-
- private:
-    SocketBreaker(const SocketBreaker&);
-    SocketBreaker& operator=(const SocketBreaker&);
-
- private:
-    Mutex m_mutex;
-    WSAEVENT m_event;
-    bool m_create_success;
-    bool m_broken;
-    int m_exception;
-    int m_reason;
-};
 
 class SocketSelect {
  public:
