7e260fc0e373715ceb4fff286e0a97ff080ea0d8
cpan <cpan@tencent.com>
2023-10-11 17:31:22 +0800

    [mars]fix windows build cann't import boost any

--- mars/app/app_manager.h
+++ mars/app/app_manager.h
@@ -10,7 +10,7 @@
 #include <unordered_map>
 
 #include "mars/app/app.h"
-#include "mars/boost/any.hpp"
+//#include "mars/boost/any.hpp"
 #include "mars/boot/base_manager.h"
 #include "mars/boot/context.h"
 #include "mars/comm/alarm.h"
@@ -59,14 +59,15 @@ class AppManager : public mars::boot::BaseManager {
             xwarn2(TSF "AppConfig GetConfig return default value. ");
             return default_value;
         }
-        return boost::any_cast<T>(it->second);
+        // return boost::any_cast<T>(it->second);
+        return *static_cast<T*>(it->second);
     }
 
     template <typename T>
     void SetConfig(const std::string& key, T value) {
         xinfo2(TSF "AppConfig SetConfig key:%_, value:%_", key, value);
         std::unique_lock<std::mutex> lock(mutex_);
-        config_[key] = value;
+        config_[key] = new T(value);
         types_[key] = std::type_index(typeid(T)).name();
         lock.unlock();
         __CheckCommSetting(key);
@@ -95,7 +96,8 @@ class AppManager : public mars::boot::BaseManager {
     int slproxycount_ = 0;
 
     std::mutex mutex_;
-    std::unordered_map<std::string, boost::any> config_;
+    // std::unordered_map<std::string, boost::any> config_;
+    std::unordered_map<std::string, void*> config_;
     std::unordered_map<std::string, std::string> types_;
 };
 
