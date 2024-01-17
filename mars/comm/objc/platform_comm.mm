43fd1029a5ff95efa5468b9578fede701f2b623a
tiemuhuaguo <tiemuhuaguo@tencent.com>
2024-01-17 16:50:51 +0800

    fix macos build

--- mars/comm/objc/platform_comm.mm
+++ mars/comm/objc/platform_comm.mm
@@ -464,7 +464,7 @@ bool getCurSIMInfo(mars::comm::SIMInfo& simInfo, bool /* realtime = false*/) {
 
 #else
 
-bool getCurSIMInfo(SIMInfo& simInfo) { return false; }
+bool getCurSIMInfo(mars::comm::SIMInfo& simInfo, bool /* realtime = false*/) { return false; }
 #endif
 
 bool getAPNInfo(mars::comm::APNInfo& info) {
