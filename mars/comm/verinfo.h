c0bf697203ac2e2d932e91491112b241f9950d36
perryzhou <perryzhou@tencent.com>
2024-03-22 17:14:52 +0800

    upgrade googletest

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "b7c30daa"
-#define MARS_PATH "release/android/2024-t1.1"
+#define MARS_REVISION "b57ae091"
+#define MARS_PATH "ft/android/2024-t1.1-connectcfg"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2024-03-21 20:07:15"
+#define MARS_BUILD_TIME "2024-03-22 16:09:46"
 #define MARS_TAG ""
 
 #endif
--- mars/googlemock/include/gmock/gmock.h
+++ mars/googlemock/include/gmock/gmock.h
@@ -27,12 +27,9 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
 // Google Mock - a framework for writing C++ mock classes.
 //
 // This is the main header file a user should include.
-
-// GOOGLETEST_CM0002 DO NOT DELETE
 
 #ifndef GOOGLEMOCK_INCLUDE_GMOCK_GMOCK_H_
 #define GOOGLEMOCK_INCLUDE_GMOCK_GMOCK_H_
@@ -64,13 +61,14 @@
 #include "gmock/gmock-more-matchers.h"
 #include "gmock/gmock-nice-strict.h"
 #include "gmock/internal/gmock-internal-utils.h"
-
-namespace testing {
+#include "gmock/internal/gmock-port.h"
 
 // Declares Google Mock flags that we want a user to use programmatically.
 GMOCK_DECLARE_bool_(catch_leaked_mocks);
 GMOCK_DECLARE_string_(verbose);
 GMOCK_DECLARE_int32_(default_mock_behavior);
+
+namespace testing {
 
 // Initializes Google Mock.  This must be called before running the
 // tests.  In particular, it parses the command line for the flags
