db410e2bbaf295f36a5694d832cb4ab76fcf06ef
tiemuhuaguo <tiemuhuaguo@tencent.com>
2024-03-27 11:36:07 +0800

    Revert "NO MERGE!!! openssl适配鸿蒙"
    This reverts commit e2515c20a0fcbb8f9bf5eabba20e5b79ae65dc05.

--- mars/openssl/include/openssl/opensslconf.h
+++ mars/openssl/include/openssl/opensslconf.h
@@ -5,14 +5,13 @@
 
  * See also https://github.com/x2on/OpenSSL-for-iPhone/issues/126 and referenced pull requests
  */
-#include <openssl/opensslconf-android.h>
 
-//#ifdef __APPLE__
-//#include <openssl/opensslconf-apple.h>
-//#elif defined(__ANDROID__)
-//#include <openssl/opensslconf-android.h>
-//#elif defined(__WIN32__)
-//#include <openssl/opensslconf-windows.h>
-//#else
-//#error unsupported platform
-//#endif
+#ifdef __APPLE__
+#include <openssl/opensslconf-apple.h>
+#elif defined(__ANDROID__)
+#include <openssl/opensslconf-android.h>
+#elif defined(__WIN32__)
+#include <openssl/opensslconf-windows.h>
+#else
+#error unsupported platform
+#endif
