dd7f7ad064e4ce955f1ee5d418857e6c29edb96e
perryzhou <perryzhou@tencent.com>
2024-05-10 15:48:34 +0800

    build timestamp

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,11 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "13e1ec55"
-#define MARS_PATH "release/android/2024-t1"
+#define MARS_REVISION "293a1894"
+#define MARS_PATH "ft/android/2024-t2-cdntls"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2024-02-28 11:22:06"
+#define MARS_BUILD_TIME "2024-05-10 15:13:15"
 #define MARS_TAG ""
+#define MARS_BUILD_TIMESTAMP 1715325195
 
 #endif
