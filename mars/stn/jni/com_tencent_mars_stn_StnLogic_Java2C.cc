7b06d64fcfb67477837994344a04e6c4abb56a9a
austinjiang <austinjiang@tencent.com>
2024-06-27 14:37:08 +0800

    refactor: host_extra_info改名为extra_info

--- mars/stn/jni/com_tencent_mars_stn_StnLogic_Java2C.cc
+++ mars/stn/jni/com_tencent_mars_stn_StnLogic_Java2C.cc
@@ -208,8 +208,8 @@ JNIEXPORT void JNICALL Java_com_tencent_mars_stn_StnLogic_startTask(JNIEnv* _env
 
     jboolean needRealtimeNetInfo = JNU_GetField(_env, _task, "needRealtimeNetInfo", "Z").z;
 
-    jobject oHostExtraInfo = JNU_GetField(_env, _task, "hostExtraInfo", "Ljava/util/Map;").l;
-    std::map<std::string, std::string> host_extra_info = JNU_JObject2Map(_env, oHostExtraInfo);
+    jobject extraInfo = JNU_GetField(_env, _task, "extraInfo", "Ljava/util/Map;").l;
+    std::map<std::string, std::string> extra_info = JNU_JObject2Map(_env, extraInfo);
 
     // init struct Task
     struct Task task(taskid);
@@ -265,7 +265,7 @@ JNIEXPORT void JNICALL Java_com_tencent_mars_stn_StnLogic_startTask(JNIEnv* _env
     }
     task.client_sequence_id = client_sequence_id;
     task.need_realtime_netinfo = (needRealtimeNetInfo == JNI_TRUE);
-    task.host_extra_info = host_extra_info;
+    task.extra_info = extra_info;
 
     StartTask(task);
 }
