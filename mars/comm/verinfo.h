272ab70bd70d37596671de609c0351170ab37b81
perryzhou <perryzhou@tencent.com>
2024-07-30 17:05:48 +0800

    update version

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,11 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "13e1ec55"
-#define MARS_PATH "release/android/2024-t1"
+#define MARS_REVISION "38eea3ee"
+#define MARS_PATH "release/2024-t2.1"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2024-02-28 11:22:06"
+#define MARS_BUILD_TIME "2024-07-30 17:02:21"
 #define MARS_TAG ""
+#define MARS_BUILD_TIMESTAMP 1722330141
 
 #endif
