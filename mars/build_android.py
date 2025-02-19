a9711f842de21fe84b412cb003c1ae9bafbd2016
Mao Han <han_mao@linux.alibaba.com>
2025-02-19 13:42:49 +0800

    Add RISC-V 64 support to build infrastructure
    This commit updates build_android.py to support RISC-V architecture
    by adding riscv64 to the strip and STL file paths, and setting the
    appropriate API level (android-35) for RISC-V builds.
    The -Wno-deprecated-declarations and -Wno-deprecated-builtins options
    have been added to suppress warnings generated by newer versions of NDK.

--- mars/build_android.py
+++ mars/build_android.py
@@ -34,7 +34,7 @@ ANDROID_LIBS_INSTALL_PATH = BUILD_OUT_PATH + '/'
 ANDROID_BUILD_CMD = 'cmake "%s" %s -DANDROID_ABI="%s" ' \
                     '-DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%s/build/cmake/android.toolchain.cmake ' \
                     '-DANDROID_TOOLCHAIN=clang -DANDROID_NDK=%s ' \
-                    '-DANDROID_PLATFORM=android-21 ' \
+                    '-DANDROID_PLATFORM=%s ' \
                     '-DANDROID_STL="c++_shared" ' \
                     '&& cmake --build . %s --config Release -- -j8'
 ANDROID_SYMBOL_PATH = 'libraries/mars_android_sdk/obj/local/'
@@ -49,6 +49,7 @@ ANDROID_STRIP_FILE = {
         'x86': NDK_ROOT + '/toolchains/x86-4.9/prebuilt/%s/bin/i686-linux-android-strip',
         'arm64-v8a': NDK_ROOT + '/toolchains/aarch64-linux-android-4.9/prebuilt/%s/bin/aarch64-linux-android-strip',
         'x86_64': NDK_ROOT + '/toolchains/x86_64-4.9/prebuilt/%s/bin/x86_64-linux-android-strip',
+        'riscv64': NDK_ROOT + '/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip',
          }
 
 
@@ -58,6 +59,7 @@ ANDROID_STL_FILE = {
         'x86': NDK_ROOT + '/sources/cxx-stl/llvm-libc++/libs/x86/libc++_shared.so',
         'arm64-v8a': NDK_ROOT + '/sources/cxx-stl/llvm-libc++/libs/arm64-v8a/libc++_shared.so',
         'x86_64': NDK_ROOT + '/sources/cxx-stl/llvm-libc++/libs/x86_64/libc++_shared.so',
+        'riscv64': NDK_ROOT + '/toolchains/llvm/prebuilt/linux-x86_64/android_libc++/ndk/riscv64/lib/libc++_shared.so',
         }
 
 
@@ -68,8 +70,10 @@ def get_android_strip_cmd(arch):
         system_str = system_str + '-x86_64'
     else:
         pass
-
-    strip_cmd = ANDROID_STRIP_FILE[arch] %(system_str)
+    if (arch == 'riscv64'):
+        strip_cmd = ANDROID_STRIP_FILE[arch]
+    else:
+        strip_cmd = ANDROID_STRIP_FILE[arch] %(system_str)
     print('Android strip cmd:%s' %(strip_cmd))
     return strip_cmd
 
@@ -80,8 +84,13 @@ def build_android(incremental, arch, target_option=''):
     
     clean(BUILD_OUT_PATH, incremental)
     os.chdir(BUILD_OUT_PATH)
-    
-    build_cmd = ANDROID_BUILD_CMD %(SCRIPT_PATH, ANDROID_GENERATOR, arch, NDK_ROOT, NDK_ROOT, target_option)
+
+    if (arch == 'riscv64'):
+        API_LEVEL = 'android-35'
+    else:
+        API_LEVEL = 'android-21'
+
+    build_cmd = ANDROID_BUILD_CMD %(SCRIPT_PATH, ANDROID_GENERATOR, arch, NDK_ROOT, NDK_ROOT, API_LEVEL, target_option)
     print("build cmd:" + build_cmd)
     ret = os.system(build_cmd)
     os.chdir(SCRIPT_PATH)
