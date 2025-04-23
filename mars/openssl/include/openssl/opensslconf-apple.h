8934f4907b1c48db9a2a5ce156ba38d6220e51c9
orangeboyChen <chenenhan@qq.com>
2025-04-24 01:35:18 +0800

    feat: support xcframework for watchOS target

--- mars/openssl/include/openssl/opensslconf-apple.h
+++ mars/openssl/include/openssl/opensslconf-apple.h
@@ -18,6 +18,8 @@
 # include <openssl/opensslconf_watchos_arm64_32.h>
 #elif TARGET_OS_WATCH && TARGET_OS_SIMULATOR && TARGET_CPU_X86
 # include <openssl/opensslconf_watchos_sim_i386.h>
+#elif TARGET_OS_WATCH && TARGET_OS_SIMULATOR && TARGET_CPU_X86_64
+# include <openssl/opensslconf_watchos_sim_x86_64.h>
 #elif TARGET_OS_OSX && TARGET_CPU_X86_64
 # include <openssl/opensslconf_macos_x86_64.h>
 #elif TARGET_OS_OSX && TARGET_CPU_ARM64
